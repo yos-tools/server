c203b7c2560865e8d5294675bb2e389c4797dc96
Kai Haase <kai.haase@pw-pro.de>
2018-08-02 03:50:37 +0200

    Hint for YosObject

--- src/interfaces/yos-object.interface.ts
+++ src/interfaces/yos-object.interface.ts
@@ -1,5 +1,15 @@
 /**
- * Interface for a standard key value Object
+ * Interface for a standard key value Javascript Object
+ *
+ * The interface is required in case "noImplicitAny": true is set in tsconfig.json:
+ *
+ * x: object = <any>{};
+ * x['test'] = 'xxx'; // => TS7017: Element implicitly has an 'any' type because type '{}' has no index signature.
+ * x.test = 'xxx'; // => TS2339: Property 'x' does not exist on type 'object'.
+ *
+ * x: YosObject = {};
+ * x['test'] = 'xxx' // => works
+ * x.test = 'xxx' // => works
  */
 export interface YosObject {
 
